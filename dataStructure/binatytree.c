3dc3d0a9a6aaf428938a0adaed6477bcc4082ee3
eikhyeonchoi <eikhyeon.choi@gmail.com>
2023-01-11 17:19:32 +0900

    Update binatytree.c
    delete 기능(함수) 수정

--- dataStructure/binatytree.c
+++ dataStructure/binatytree.c
@@ -1,158 +1,188 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// 참고
-// https://code-lab1.tistory.com/10
 
-typedef struct _Node {
-	int val;
-	struct _Node* left;
-	struct _Node* right;
-}Node;
+typedef struct _TreeNode {
+    int key;    // key 값
+    _TreeNode* left;    // left child
+    _TreeNode* right;     // right child
+}TreeNode;
 
-Node* search(Node*, int);
-Node* insert(Node*, int);
+TreeNode* search(TreeNode* root, int key) {
+    if (root == NULL) {    // 값을 찾지 못한 경우  
+        printf("Error : 값을 찾을 수 없습니다\n");
+        return root;
+    }
+
+    if (key == root->key) {    // 값을 찾음 
+        return root;
+    }
+    else if (key < root->key) {    // 왼쪽 서브트리 탐색 
+        search(root->left, key);
+    }
+    else if (key > root->key) {    // 오른쪽 서브트리 탐색 
+        search(root->right, key);
+    }
+
+}
+
+
+TreeNode* insert(TreeNode* root, int key) {
+    TreeNode* ptr;     // 탐색을 진행할 포인터 
+    TreeNode* newNode = (TreeNode*)malloc(sizeof(TreeNode));    // newNode 생성
+    newNode->key = key;
+    newNode->left = newNode->right = NULL;
+
+    if (root == NULL) {    // 트리가 비어 있을 경우 
+        root = newNode;
+        return root;
+    }
+
+    ptr = root;    // root 노드부터 탐색 진행  
+
+    while (ptr) {
+        if (key == ptr->key) {    // 중복값 
+            printf("Error : 중복값은 허용되지 않습니다!\n");
+            return root;
+        }
+        else if (key < ptr->key) {    // 왼쪽 서브트리 
+            if (ptr->left == NULL) {    // 비어있다면 추가 
+                ptr->left = newNode;
+                return root;
+            }
+            else {    // 비어있지 않다면 다시 탐색 진행 
+                ptr = ptr->left;
+            }
+        }
+        else {    // key > ptr->key 오른쪽 서브트리 
+            if (ptr->right == NULL) {    // 비어있다면 추가 
+                ptr->right = newNode;
+                return root;
+            }
+            else {    // 비어있지 않다면 다시 탐색 진행 
+                ptr = ptr->right;
+            }
+        }
+    }
+
+}
+
+
+TreeNode* delete_node(TreeNode* root, int key) {
+    TreeNode* del = NULL;    // 삭제할 노드     
+    TreeNode* parent = NULL;    // 삭제할 노드의 부모 노드 
+    TreeNode* successor = NULL;    // 삭제할 노드의 왼쪽 서브트리에서 가장 큰 노드 
+    TreeNode* predecessor = NULL;    // successor의 부모노드 
+    TreeNode* child = NULL;        // 삭제할 노드의 자식 노드 
+
+    del = root;
+    while (del != NULL) {    // 삭제할 노드 탐색 
+        if (key == del->key) {
+            break;
+        }
+        parent = del;
+        if (key < del->key) {
+            del = del->left;
+        }
+        else {
+            del = del->right;
+        }
+    }
+
+    if (del == NULL) {
+        printf("Error : 존재하지 않는 키\n");
+        return root;
+    }
+
+    if (del->left == NULL && del->right == NULL) {    // 삭제할 노드의 자식노드가 없는 경우 
+        if (parent != NULL) {    // 부모노드가 있는 경우 
+            if (parent->left == del) {    // 부모노드의 왼쪽노드가 삭제할 노드일 때 
+                parent->left = NULL;
+            }
+            else {    // 오른쪽 일 때 
+                parent->right = NULL;
+            }
+        }
+        else {    // 부모노드가 없는 경우 = root 노드 
+            root = NULL;
+        }
+    }
+    else if (del->left != NULL && del->right != NULL) {    // 삭제할 노드의 자식 노드가 2개인 경우 
+        predecessor = del;
+        successor = del->left;
+
+        while (successor->right != NULL) {    // 왼쪽 서브트리에서 가장 큰 값 찾기 
+            predecessor = successor;
+            successor = successor->right;
+        }
+
+        if (parent != NULL) {
+            predecessor->right = successor->left;
+            successor->left = del->left;
+            successor->right = del->right;
+            if (parent->left == del) {
+                parent->left = successor;
+            }
+            else {
+                parent->right = successor;
+            }
+        } else {
+            if (predecessor != del) {
+                predecessor->right = successor->left;
+                successor->right = del->right;
+                successor->left = predecessor;
+            } else {
+                successor->right = predecessor->right;
+            }
+
+            root = successor;
+        }
+    } else {    //     삭제할 노드의 자식 노드가 1개인 경우 
+        if (del->left != NULL) {    // 왼쪽 노드 
+            child = del->left;
+        }
+        else {    // 오른쪽 노드 
+            child = del->right;
+        }
+
+        if (parent != NULL) {    // 부모노드가 있는 경우 
+            if (parent->left == del) {    // 부모노드의 왼쪽 노드로 삭제할 노드의 자식노드 연결 
+                parent->left = child;
+            }
+            else {    // 부모노드의 오른쪽 노드로 삭제할 노드의 자식노드 연결  
+                parent->right = child;
+            }
+        }
+        else {
+            root = child;
+        }
+    }
+
+    free(del);    // 메모리해제 
+    return root;
+}
+
+
+void print_tree(TreeNode* root) {
+    if (root == NULL) {
+        return;
+    }
+    printf("%d\n", root->key);
+    print_tree(root->left);
+    print_tree(root->right);
+}
 
 int main() {
-	Node* root = NULL;
-
-	return 0;
-}
-
-Node* search(Node* node, int key) {
-	if (!node) {
-		printf("empty tree\n");
-		return NULL;
-	}
-
-	if (node->val == key) {
-		return node;
-	}
-
-	if (node->val < key) {
-		return search(node->right, key);
-	}
-	else {
-		return search(node->left, key);
-	}
-}
-
-Node* insert(Node* node, int key) {
-	Node* newNode = (Node*)malloc(sizeof(Node));
-	newNode->val = key;
-	newNode->left = newNode->right = NULL;
-
-	if (!node) {
-		node = newNode;
-		return node;
-	}
-
-	Node* ptr = node;
-	while (ptr) {
-		if (ptr->val == key) {
-			printf("중복된 value = '%d'\n", key);
-			return node;
-		}
-
-		if (ptr->val < key) {
-			if (ptr->right) {
-				ptr = ptr->right;
-			}
-			else {
-				ptr->right = node;
-				return node;
-			}
-		}
-		else {
-			if (ptr->left) {
-				ptr = ptr->left;
-			}
-			else {
-				ptr->left = node;
-				return node;
-			}
-		}
-	}
-}
-
-Node* deleteNode(Node* node, int key) {
-	Node* parent = NULL;
-	Node* del = node;
-	while (del) {
-		if (del->val == key) {
-			break;
-		}
-
-		parent = del;
-		if (del->val > key) {
-			del = del->left;
-		}
-		else {
-			del = del->right;
-		}
-	}
-
-	if (!del) {
-		printf("no node\n");
-		return NULL;
-	}
-
-	// 단말노드일경우 단순 삭제
-	if (!(del->right) && !(del->left)) {
-		if (parent) {
-			if (parent->left == del) {
-				parent->left = NULL;
-			} else {
-				parent->right = NULL;
-			}
-		} else {
-			node = NULL;
-		}
-	}
-
-	// 자식노드가 하나일경우
-	if (!(del->right) || !(del->left)) {
-		Node* child = del->left;
-		if (!del->left) {
-			child = del->right;
-		}
-
-		if (parent) {
-			if (parent->left == del) {
-				parent->left = child;
-			} else {
-				parent->right = child;
-			}
-		} else {
-			node = child;
-		}
-	}
-
-	// 자식노드가 2개일 경우
-	if (del->left && del->right) {
-		Node* leftParent = del;
-		Node* leftChild = del->left;
-
-		while (leftChild->right) {
-			leftParent = leftChild;
-			leftChild = leftChild->right;
-		}
-
-		leftParent->right = leftChild->left;
-		leftChild->left = node->left;
-		leftChild->right = node->right;
-
-		if (parent) {
-			if (parent->left == del) {
-				parent->left = leftChild;
-			} else {
-				parent->right = leftChild;
-			}
-		} else {
-			node = leftChild;
-		}
-	}
-
-	free(del);
-	return node;
+    TreeNode* root = NULL;
+    TreeNode* ptr = NULL;
+    root = insert(root, 20);
+    root = insert(root, 10);
+    root = insert(root, 5);
+    root = insert(root, 15);
+    root = insert(root, 13);
+    root = insert(root, 30);
+    root = delete_node(root, 20);
+    print_tree(root);
+    printf("\n");
+    return 0;
 }
